6a86f67fcf395e58f4a0f208e781272e13a8ae6c
deka108 <deka108@gmail.com>
2017-02-18 10:42:15 +0800

    Initial UI for homepage + Added filter and order by controls

--- viator/ui/src/controllers/SearchController.js
+++ viator/ui/src/controllers/SearchController.js
@@ -1,5 +1,23 @@
 function SearchController($scope) {
+    $scope.searchFilters = [{
+            id: 1,
+            category: 'Emotions',
+            values: ['Happy', 'Sad', 'Angry']
+        },
+        {
+            id: 2,
+            category: 'Country',
+            values: ['Indonesia', 'Japan', 'Malaysia', 'Singapore']
+        }
+    ];
+    $scope.searchOrders = [
+        { id: 1, value: 'Likes' },
+        { id: 2, value: 'Country' }
+    ];
 
+    $scope.update = function() {
+        console.log($scope.selectedFilterCategory);
+    }
 }
 
 export default ['$scope', SearchController];
