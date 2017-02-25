72cf2175cb9585700572d1d97507f42961d4ac5e
deka108 <deka108@gmail.com>
2017-02-25 10:25:45 +0800

    Indexing json with solr

--- viator/server/handler/db_manager.py
+++ viator/server/handler/db_manager.py
@@ -6,13 +6,32 @@ import requests
 
 db_manager = Blueprint('db_manager', __name__, static_folder='../data')
 
-@db_manager.route('/indexing/<country>')
-def indexing_database(country):
-    file_name = util.get_file_path(country)
-    if file_name:
-        # r = requests.post(config.SOLR_UPDATE_JSON_URL, json=data)
-        return "success!"
+
+@db_manager.route('/indexing/')
+def indexing_all_database():
+    countries = util.get_data_names()
+    for country in countries:
+        data = util.read_json_data(country)
+        if data:
+            try:
+                r = requests.post(config.SOLR_UPDATE_JSON_URL, json=data)
+                return "{} successfully indexed!".format(country)
+            except requests.exceptions.ConnectionError: 
+                return "Unable to connect with Solr server"
     return "Country not exist"
+
+
+@db_manager.route('/indexing/<country>')
+def indexing_country_database(country):
+    data = util.read_json_data(country)
+    if data:
+        try:
+            r = requests.post(config.SOLR_UPDATE_JSON_URL, json=data)
+            return "{} successfully indexed!".format(country)
+        except requests.exceptions.ConnectionError: 
+            return "Unable to connect with Solr server"
+    return "Country not exist"
+
 
 @db_manager.route('/update/')
 def update_database():
@@ -38,6 +57,7 @@ def read_data(country):
     if file_name:
         return db_manager.send_static_file(file_name)
     return "Country does not exist"
+
 
 def reindex_database():
     pass
--- viator/server/views/util.py
+++ viator/server/views/util.py
@@ -24,12 +24,14 @@ def get_data_names():
 
 def get_file_path(country):
     if country in DATA_MAP:
-        return config.get_data_path(country)
+        return config.get_data_path(DATA_MAP[country])
 
 def get_file_name(country):
     if country in DATA_MAP:
         return DATA_MAP[country]
 
-def read_json_data(data_path):
-    with open(data_path) as data_file:
-        return json.load(data_file)
+def read_json_data(country):
+    data_path = get_file_path(country)
+    if data_path:
+        with open(data_path) as data_file:
+            return json.load(data_file)
