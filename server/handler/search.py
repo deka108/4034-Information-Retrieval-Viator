01215d319be6c830541cea26c784a85009303839
albertlucianto <albertlucianto@gmail.com>
2017-03-28 12:37:54 +0800

    add pagination

--- server/handler/search.py
+++ server/handler/search.py
@@ -12,8 +12,9 @@ def search_query():
     request_data = json.loads(request.get_data())
     if request_data:
         query_params = request_data.get('q')
+        page_params = request_data.get('p')
 
-    response = solr_interface.search(query_params)
+    response = solr_interface.search(query_params,page_params)
     return jsonify(response)
 
     return abort(404)
