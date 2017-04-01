e416d64a46beec402133cb959602bce9634647d4
gracechristina <gracechristina05@gmail.com>
2017-04-01 14:25:31 +0800

    sentiment per page id

--- run_script.py
+++ run_script.py
@@ -8,8 +8,8 @@ from server.core.data_preprocessing import statistic
 def run():
     preprocessing.preprocess_all_pages()
     # generate_csv.generate_splitted_csv_for_labelling()
-    # statistic.compute_words(du.ALL_POSTS_COMMENTS_FILENAME)
-    # nlp_main.run()
+    statistic.compute_words(du.ALL_POSTS_COMMENTS_FILENAME)
+    nlp_main.run()
 
     # Example of getting json based on page_id
     # page_id = "koreatourism"
