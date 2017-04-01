0f41263ddfec60d08f797907b0d040771d810cac
gracechristina <gracechristina05@gmail.com>
2017-04-01 11:07:07 +0800

    updated

--- server/core/data_preprocessing/statistic.py
+++ server/core/data_preprocessing/statistic.py
@@ -3,8 +3,8 @@ from server.utils import data_util,text_util
 import pandas as pd
 
 def compute_words(file_name):
-    data_path = data_util.get_csv_filepath(file_name)
-    df = data_util.get_csv_data(data_path)
+    # data_path = data_util.get_csv_filepath(file_name)
+    df = data_util.get_csv_data_from_filename(file_name)
     df["message_cleaned"] = df["message"].apply(
         lambda x: text_util.clean_text(x) if pd.notnull(x) else "")
     df["description_cleaned"] = df["description"].apply(
