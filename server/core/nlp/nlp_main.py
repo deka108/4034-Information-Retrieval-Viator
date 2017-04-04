91ab59746f714d488393f037a516e433071aa0c6
gracechristina <gracechristina05@gmail.com>
2017-04-04 20:40:59 +0800

    update

--- server/core/nlp/nlp_main.py
+++ server/core/nlp/nlp_main.py
@@ -2,8 +2,8 @@ from server.core.nlp import sentiment, extract_geocoding
 
 
 def run():
-    # sentiment.run()
-    extract_geocoding.run()
+    sentiment.run()
+    # extract_geocoding.run()
     #location_ner_stanford.run()
 
 
